37d3b9e87e2577bf931482a587c5d39410ac8f4f
banana0816 <Zzhu@putnamscience.org>
2025-05-09 09:07:22 -0400

    Update finalprojectharrison.py

--- finalprojectharrison.py
+++ finalprojectharrison.py
@@ -4,20 +4,20 @@ class books():
         self.name = ""
         self.title = ""
         self.price = 0
-        self._Books = {"sb"}
+        self._Books = {}
 
     def add(self):
         self.title = input("What book would u like to add")
         print("You have added",self.title)
 
-        self._name = input("enter the name of the book you would like to add: \n").upper
-        self._author = input("enter the author of the book you have just added: \n").upper
-        self.price = float(input("enter the listing price  of the book: \n"))
+        self._name = input("enter the name of the book you would like to add: \n").upper()
+        self._author = input("enter the author of the book you have just added: \n").upper()
+        self.price = float(input("enter the listing price  of the book: \n")) 
 
 
         
-        self.Books[self._name] = {'author':self._author,'price':self._price}
-
+        self.Books[self._name] = {"author":self._author,"price":self._price}
+        
 
 
     def sell(self):
@@ -33,6 +33,15 @@ class books():
     def show(self):
         for x in self.Books:
             print(x)
-            print(self.Books)[x]
-            print(self.Books)[x]["author"]            
-            print(self.Books)[x]["price"]
+            print(self.Books[x])
+            print(self.Books[x])["author"]            
+            print(self.Books[x])["price"]
+
+
+            def save(self,filename = "books.txt"):
+                with open(filename,'w')as file:
+                    for book, info in self.Books.items():
+                        file.write(f"{book},{info['author']},{info['price']}\n")
+                    print("Books saved to file.")
+
+                    
