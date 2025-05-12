eefcad6d9f1631db223050228d2c8d030f98a680
banana0816 <Zzhu@putnamscience.org>
2025-05-12 15:31:24 -0400

    1

--- finalprojectharrison.py
+++ finalprojectharrison.py
@@ -1,23 +1,21 @@
-class books():
+class books(): 
     def __init__(self):
-        self.author = "harrison"
-        self.name = ""
-        self.title = ""
-        self.price = 0
-        self._Books = {}
+                self.author = "harrison"
+                self.name = ""
+                self.title = ""
+                self.price = 0
+                self.Books = {}
 
     def add(self):
         self.title = input("What book would u like to add")
-        print("You have added",self.title)
+        print("You have added", self.title)
 
-        self._name = input("enter the name of the book you would like to add: \n").upper()
-        self._author = input("enter the author of the book you have just added: \n").upper()
-        self.price = float(input("enter the listing price  of the book: \n")) 
-
-
+        self.name = input("enter the name of the book you would like to add: \n").upper()
+        self.author = input("enter the author of the book you have just added: \n").upper()
+        self.price = float(input("enter the listing price  of the book: \n"))
         
-        self.Books[self._name] = {"author":self._author,"price":self._price}
-        
+        self.Books[self.name] = {"author" : self.author,"price" : self.price}
+
 
 
     def sell(self):
@@ -25,36 +23,36 @@ class books():
         try:
             str(self.name)
             if self.name in self.Books:
-                self.Books.pop(self.name)  
+                self.Books.pop(self.name)
                 print("the remaining books are:",self.Books)
         except:
             print("please enter a book title")
 
     def show(self):
-        for x in self.Books:
-            print(x)
-            print(self.Books[x])
-            print(self.Books[x])["author"]            
-            print(self.Books[x])["price"]
+        for i in self.Books:
+            print(i)#0
+            print(self.Books[i])#harry potter
+            print(self.Books[i]["author"])#J.K.Rowling         
+            print(self.Books[i]["price"])#1
 
+    def Save(self, filename = "books.txt"):
+        with open(filename, 'w') as file:
+            for i in self.Books:
+                file.write(f"{self.Books[i]}, ")
+                file.write(f"{self.Books[i]["author"]}, ")
+                file.write(f"{self.Books[i]["price"]}\n")
+        print("Books saved to file.")
 
-            def save(self,filename = "books.txt"):
-                with open(filename,'w')as file:
-                    for book, info in self.Books.items():
-                        file.write(f"{book},{info['author']},{info['price']}\n")
-                    print("Books saved to file.")
+    def Load(self, filename = "books.txt"):
+        try:
+            with open(filename, 'r') as file:
 
-
-            def load(self,filename = "booksrore.txt"):
-                try:
-                    with open(filename,'r')as file:
-
-                        for line in file:
-                            title,author,price = line.strip().split(',')
-                            self.Books[title]={'author':author,'price':float(price)}
-                    print("Books loaded from file.")
-                except: FileNotFoundError
-                print("FileNotFoundError")
+                for line in file:
+                    title, author, price = line.strip().split(', ')
+                    self.Books[title] = {'author': author,'price': float(price)}
+            print("Books loaded from file.")
+        except FileNotFoundError:
+            print("File not found.")
 
       
 
--- finalprojectharrison2.py
+++ finalprojectharrison2.py
@@ -1,8 +1,29 @@
-
 from finalprojectharrison import books
 b = books()
+
+def Load(self, filename = "books.txt"):
+        try:
+            with open(filename, 'r') as file:
+
+                for line in file:
+                    title, author, price = line.strip().split(', ')
+                    self.Books[title] = {'author': author,'price': float(price)}
+            print("Books loaded from file.")
+        except FileNotFoundError:
+            print("File not found.")
+
+Load()
 
 print(b.name)
 v = input("would u like to add a book?('yes','no')")
 if v == "yes":
     c = b.add()
+v = input("would u like to remove a book?('yes','no')")
+if v == "yes":
+    c = b.sell()
+v = input("would u like to see the books?('yes','no')")
+if v == "yes":
+    c = b.show()
+v = input("would u like to save the books?('yes','no')")
+if v == "yes":
+    c = b.Save()
